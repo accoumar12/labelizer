d30359fa4500da454aaac376a7cdef9977054245
Martin ACCOU <martin.accou@icloud.com>
2026-04-06 16:27:08 +0100

    fix: vite proxy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api/labelizer": {
-        target: "http://127.0.0.1:42042",
+        target: process.env.VITE_API_URL || "http://127.0.0.1:42042",
         changeOrigin: true,
       },
     },
